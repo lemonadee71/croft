31928a394a0bb1530f7a0966075d443d5a76ec73
Shin Andrei Riego <riegoshinandrei@gmail.com>
2026-07-06 19:49:40 +0800

    Fix test setup

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,2 @@
-import { expect } from "vitest";
-import * as matchers from "@testing-library/jest-dom/matchers";
-
-// Extend Vitest's expect with jest-dom matchers
-expect.extend(matchers);
+import "@testing-library/jest-dom/vitest";
 
