125bbc27723022820c229e4b073e5a5e022ea2c6
Shin Andrei Riego <riegoshinandrei@gmail.com>
2026-07-10 20:40:09 +0800

    docs: Add CHANGELOG to docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -87,6 +87,7 @@ export default defineConfig({
         text: "Reference",
         items: [
           { text: "API Reference", link: "/api-reference" },
+          { text: "Changelog", link: "/changelog" },
         ],
       },
     ],
